f157f833ad53a3e1699aa50a85cbd265470df7d2
root <root@mesos-base-node.ebrandvalue.net>
2015-07-04 19:59:26 +0000

    canceled data store and webmin, added first env variables FORWARDERS and DNSSEC_VALIDATION

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,18 @@
 FROM sameersbn/ubuntu:14.04.20150613
-MAINTAINER sameer@damagehead.com
+MAINTAINER erhankesken@gmail.com
 
-ENV DATA_DIR=/data \
-    BIND_USER=bind \
-    WEBMIN_VERSION=1.760
+ENV BIND_USER=bind \
+ FORWARDERS="8.8.8.8;" \
+ DNSSEC_VALIDATION=auto
 
 RUN rm -rf /etc/apt/apt.conf.d/docker-gzip-indexes \
  && apt-get update \
- && apt-get install -y bind9 perl libnet-ssleay-perl openssl \
-      libauthen-pam-perl libpam-runtime libio-pty-perl \
-      apt-show-versions python pwgen \
- && wget "http://prdownloads.sourceforge.net/webadmin/webmin_${WEBMIN_VERSION}_all.deb" -P /tmp/ \
- && dpkg -i /tmp/webmin_${WEBMIN_VERSION}_all.deb \
- && rm -rf /tmp/webmin_${WEBMIN_VERSION}_all.deb \
+ && apt-get install -y bind9 \
  && rm -rf /var/lib/apt/lists/*
 
 COPY entrypoint.sh /sbin/entrypoint.sh
 RUN chmod 755 /sbin/entrypoint.sh
 
-EXPOSE 53/udp 10000/tcp
-VOLUME ["${DATA_DIR}"]
+EXPOSE 53/udp
 ENTRYPOINT ["/sbin/entrypoint.sh"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,59 +1,12 @@
 #!/bin/bash
 set -e
 
-# script variables
-BIND_DATA_DIR=${DATA_DIR}/bind
-WEBMIN_DATA_DIR=${DATA_DIR}/webmin
-
-# configuration variables
-WEBMIN_ENABLED=${WEBMIN_ENABLED:-true}
-
-## ...and here we go
-chmod 755 ${DATA_DIR}
-
-# create directory for bind config
-mkdir -p ${BIND_DATA_DIR}
-chown -R root:${BIND_USER} ${BIND_DATA_DIR}
-
-# populate default bind configuration if it does not exist
-if [ ! -d ${BIND_DATA_DIR}/etc ]; then
-  mv /etc/bind ${BIND_DATA_DIR}/etc
-fi
-rm -rf /etc/bind
-ln -sf ${BIND_DATA_DIR}/etc /etc/bind
-
-if [ ! -d ${BIND_DATA_DIR}/lib ]; then
-  mkdir -p ${BIND_DATA_DIR}/lib
-  chown root:${BIND_USER} ${BIND_DATA_DIR}/lib
-fi
-rm -rf /var/lib/bind
-ln -sf ${BIND_DATA_DIR}/lib /var/lib/bind
+sed -i "/^};$/i \\\tforwarders { ${FORWARDERS} };" /etc/bind/named.conf.options
+sed -i "s/dnssec-validation auto/dnssec-validation ${DNSSEC_VALIDATION}/g" /etc/bind/named.conf.options
 
 # create /var/run/named
 mkdir -m 0775 -p /var/run/named
 chown root:${BIND_USER} /var/run/named
-
-if [ "${WEBMIN_ENABLED}" == "true" ]; then
-  # create directory for webmin
-  mkdir -p ${WEBMIN_DATA_DIR}
-
-  # populate the default webmin configuration if it does not exist
-  if [ ! -d ${WEBMIN_DATA_DIR}/etc ]; then
-    mv /etc/webmin ${WEBMIN_DATA_DIR}/etc
-  fi
-  rm -rf /etc/webmin
-  ln -sf ${WEBMIN_DATA_DIR}/etc /etc/webmin
-
-  # generate a random password for root
-  if [ -z "${ROOT_PASSWORD}" ]; then
-    ROOT_PASSWORD=$(pwgen -c -n -1 12)
-    echo User: root Password: $ROOT_PASSWORD
-  fi
-  echo "root:$ROOT_PASSWORD" | chpasswd
-
-  echo "Starting webmin..."
-  /etc/init.d/webmin start
-fi
 
 if [ -z "$@" ]; then
   echo "Starting named..."
@@ -61,3 +14,4 @@ if [ -z "$@" ]; then
 else
   exec "$@"
 fi
+
